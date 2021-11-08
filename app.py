7dcdbc1a85db925fd493e2803e0752c8b7cd8733
idatewang@tamu.edu <idatewang@tamu.edu>
2021-11-07 20:48:24 -0600

    init commit 2

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = "manbearpig_MUDMAN888"
 
-@app.route("/")
+@app.route("/hello")
 def index():
-    return "Hello World!"
+	flash("what's your name?")
+	return render_template("index.html")
+
+@app.route("/greet", methods=['POST', 'GET'])
+def greeter():
+	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
+	return render_template("index.html")
